b57047c4d539cdb8fc3ae0d11d756934c8df7f90
Prem Roshan Nair <premroshan@gmail.com>
2020-09-27 16:01:19 +0530

    first 3 exercises

---
+++ ex2.py
@@ -0,0 +1,5 @@
+# i am writing a lot of commens
+print("This is a code")
+
+#this wont run
+print("This will run")
---
+++ ex3.py
@@ -0,0 +1,11 @@
+print("I will now count my chickens:")
+print("Hens", 25 + 30 / 6 )
+
+print("Roosters ", 100 -23 * 3 % 4)
+
+print("Is it true that 3 + 2 < 5 - 7?")
+print(3 + 2 < 5 - 7)
+
+print("What is 3 + 2?", 3 + 2 )
+
+print("What is 5-7?", 5-7)
