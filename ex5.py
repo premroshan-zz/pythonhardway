718ac54339b5c40c47e3478f5d75c39b6751780f
Prem Roshan Nair <premroshan@gmail.com>
2020-09-27 16:37:06 +0530

    exercise 5

---
+++ ex5.py
@@ -0,0 +1,14 @@
+my_name = "Prem Roshan Nair"
+my_age = 33
+my_height = 180
+my_weight = 100
+my_eyes = "brown"
+
+print(f"Lets talk about {my_name}")
+print(f"He is {my_age} years old")
+print(f"His height is {my_height}")
+print(f"His weight is {my_weight}")
+
+total = my_age + my_height + my_weight
+
+print(f"If we add the height and weight we get a total of {my_age} {my_weight} {my_height} = {total}")
